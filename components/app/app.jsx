92c6f3c8c9a38131b02dd06eaf9ae600b2673547
Sergei Pavlov <unplanetx@gmail.com>
2020-02-27 14:12:42 +1000

    Выполняет задание 7

--- components/app/app.jsx
+++ components/app/app.jsx
@@ -3,10 +3,13 @@ import Main from "../main/main.jsx";
 
 const App = (props) => {
   // eslint-disable-next-line react/prop-types
-  const {rentalOffersCount} = props;
+  const {rentalOffersCount, rentalOffers} = props;
 
   return (
-    <Main rentalOffersCount={rentalOffersCount} />
+    <Main
+      rentalOffersCount={rentalOffersCount}
+      rentalOffers={rentalOffers}
+    />
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ import App from "../components/app/app.jsx";
 
 const RENTAL_OFFERS_COUNT = 312;
 
+const rentalOffers = [
+  {title: `Beautiful & luxurious apartment at great location`},
+  {title: `Wood and stone place`},
+  {title: `Canal View Prinsengracht`},
+  {title: `Nice, cozy, warm big bed apartment`}
+];
+
 ReactDOM.render(
     <App
       rentalOffersCount={RENTAL_OFFERS_COUNT}
+      rentalOffers={rentalOffers}
     />,
     document.querySelector(`#root`)
 );
